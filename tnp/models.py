6539d795d87aba6cf63a3056ba9bea7a924dd655
Ethan Nitz <etnitz@gmail.com>
2022-05-27 11:18:00 -0500

    made editor model

--- tnp/models.py
+++ tnp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Editor(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
