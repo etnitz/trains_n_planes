4f1fc921850f25f3a6b39d5ec3029924c1fb4be6
Ethan Nitz <etnitz@gmail.com>
2022-05-31 10:40:06 -0500

    updated admin.py

--- tnp/admin.py
+++ tnp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Editor, Train, Plane
 
 # Register your models here.
+
+admin.site.register(Editor)
+admin.site.register(Train)
+admin.site.register(Plane)
