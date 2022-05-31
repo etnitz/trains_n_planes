092180843d4d03f394ee18d5975d40e9ee7f1d65
Ethan Nitz <etnitz@gmail.com>
2022-05-31 11:27:02 -0500

    made migrations

--- tnp/admin.py
+++ tnp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Editor, Train, Plane
+from .models import Editor, Train, Plane, Car
 
 # Register your models here.
 
 admin.site.register(Editor)
 admin.site.register(Train)
 admin.site.register(Plane)
+admin.site.register(Car)
