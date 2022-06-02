e756038c06ca2373a91f4b1b0ceeb1c3027b3009
Ethan Nitz <etnitz@gmail.com>
2022-06-02 14:53:41 -0500

    made train detail url

--- tnp/urls.py
+++ tnp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('trains/', views.train_list, name='train_list'),
     path('planes/', views.plane_list, name='plane_list'),
     path('cars/', views.car_list, name='car_list'),
-    path('boats/', views.boat_list, name='boat_list')
+    path('boats/', views.boat_list, name='boat_list'),
+    path('trains/<int:pk>', views.train_detail, name='train_detail')
 ]
