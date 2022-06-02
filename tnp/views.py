916f8d394bc2fb16a62c9ed66a267e72de0634ec
Ethan Nitz <etnitz@gmail.com>
2022-06-02 13:59:59 -0500

    made plane view

--- tnp/views.py
+++ tnp/views.py
@@ -6,3 +6,8 @@ from .models import Plane, Train, Car, Boat
 def train_list(request):
     trains = Train.objects.all()
     return render(request, 'tnp/train_list.html', {'trains':trains})
+
+def plane_list(request):
+    planes = Plane.objects.all()
+    return render(request, 'tnp/plane_list.html', {'planes':planes})
+
