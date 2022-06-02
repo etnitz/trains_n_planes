7d3c52d00032df010b85860d86594059ac643f1d
Ethan Nitz <etnitz@gmail.com>
2022-06-02 13:57:34 -0500

    started views, made train views

--- tnp/views.py
+++ tnp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Plane, Train, Car, Boat
 
 # Create your views here.
+
+def train_list(request):
+    trains = Train.objects.all()
+    return render(request, 'tnp/train_list.html', {'trains':trains})
