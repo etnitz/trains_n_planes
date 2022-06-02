eac9ea063179648a12dc9d43828bd1c04ef6dc37
Ethan Nitz <etnitz@gmail.com>
2022-06-02 17:42:07 -0500

    added car detail to urls"

--- tnp/urls.py
+++ tnp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('cars/', views.car_list, name='car_list'),
     path('boats/', views.boat_list, name='boat_list'),
     path('trains/<int:pk>', views.train_detail, name='train_detail'),
-    path('planes/<int:pk>', views.plane_detail, name='plane_detail')
+    path('planes/<int:pk>', views.plane_detail, name='plane_detail'),
+    path('cars/<int:pk>', views.car_detail, name='car_detail')
 ]
